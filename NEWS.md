be9d521c541bd3597be69228ec2a0e97badb9612
Christoph Scheuch <19249734+christophscheuch@users.noreply.github.com>
2025-02-27 15:34:27 +0100

    Initial commit

---
+++ NEWS.md
@@ -0,0 +1,3 @@
+# uisapi 0.1.0
+
+* Initial CRAN submission.
---
+++ R/uisapi-package.R
@@ -0,0 +1,10 @@
+#' @keywords internal
+#' @import cli
+#' @import httr2
+#' @import tibble
+#' @import jsonlite
+"_PACKAGE"
+
+## usethis namespace: start
+## usethis namespace: end
+NULL
